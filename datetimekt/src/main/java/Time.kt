053f31952af12235b52fccff06ccea331dc192c2
notquiteamonad <44178347+notquiteamonad@users.noreply.github.com>
2019-05-27 11:46:31 +0100

    Switch to idiomatic Kotlin constructors

--- datetimekt/src/main/java/Time.kt
+++ datetimekt/src/main/java/Time.kt
@@ -1,55 +1,20 @@
 import Consts.SECONDS_IN_AN_HOUR
-import Consts.SECONDS_IN_A_DAY
 import Consts.SECONDS_IN_A_MINUTE
 import java.time.LocalTime
 import java.util.regex.Pattern
 
-class Time private constructor (
-    private var h: Int,
-    private var m: Int,
-    private var s: Int
-): TimeType(h, m, s) {
+class Time constructor (
+        totalSeconds: Int
+): TimeType(totalSeconds, true) {
 
-    operator fun plus(other: TimeType): Time = new(this.h + other.getHours(), this.m + other.getMinutes(), this.s + other.getSeconds())
-    operator fun minus(other: TimeType): Time = new(this.h - other.getHours(), this.m - other.getMinutes(), this.s - other.getSeconds())
+    constructor(h: Int, m: Int, s: Int): this(s + SECONDS_IN_A_MINUTE * m + SECONDS_IN_AN_HOUR * h)
+
+    operator fun plus(other: TimeType): Time = Time(this.getHours() + other.getHours(), this.getMinutes() + other.getMinutes(), this.getSeconds() + other.getSeconds())
+    operator fun minus(other: TimeType): Time = Time(this.getHours() - other.getHours(), this.getMinutes() - other.getMinutes(), this.getSeconds() - other.getSeconds())
 
     companion object {
 
         private const val STRING_FORMAT_REGEX = """^\d{2}:\d{2}:\d{2}$"""
-
-        /**
-         * Produces a new Time.
-         *
-         * Times of 24 hours or greater and negative times
-         * will wrap around 24 hours to always produce a positive time.
-         *
-         * The value is calculated from total number of seconds so a time
-         * with a minute value of 90 would add an hour to the resulting time
-         * and set the minutes to 30, for example.
-         */
-        @JvmStatic
-        fun new(h: Int, m: Int, s: Int): Time {
-            val totalSeconds = s + SECONDS_IN_A_MINUTE * m + SECONDS_IN_AN_HOUR * h
-            return fromSeconds(totalSeconds)
-        }
-
-        /**
-        * Same as `TimeTuple::new()` but takes the total number of seconds
-        * as its argument and calculates the hours, minutes, and seconds
-        * from that.
-        */
-        @JvmStatic
-        @Suppress("MemberVisibilityCanBePrivate")
-        fun fromSeconds(totalSeconds: Int): Time {
-            var s = totalSeconds
-            while (s < 0) s += SECONDS_IN_A_DAY
-            while (s >= SECONDS_IN_A_DAY) s -= SECONDS_IN_A_DAY
-            val h = s / SECONDS_IN_AN_HOUR
-            s -= h * SECONDS_IN_AN_HOUR
-            val m = s / SECONDS_IN_A_MINUTE
-            s -= m * SECONDS_IN_A_MINUTE
-            return Time(h, m, s)
-        }
 
         /**
          * Converts a string in the format hh:mm:ss as would be given by
@@ -62,7 +27,7 @@ class Time private constructor (
             val pattern = Pattern.compile(STRING_FORMAT_REGEX)
             return if (pattern.matcher(string).matches()) {
                 val parts = string.split(':')
-                new(parts[0].toInt(), parts[1].toInt(), parts[2].toInt())
+                Time(parts[0].toInt(), parts[1].toInt(), parts[2].toInt())
             } else {
                 null
             }
@@ -75,7 +40,7 @@ class Time private constructor (
         @Suppress("unused")
         fun now(): Time {
             val currentTime = LocalTime.now()
-            return new(currentTime.hour, currentTime.minute, currentTime.second)
+            return Time(currentTime.hour, currentTime.minute, currentTime.second)
         }
 
     }
