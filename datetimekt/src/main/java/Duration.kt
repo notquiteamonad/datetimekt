c9acf2a0de679da8d4ba34e580236bb8b478336e
notquiteamonad <44178347+notquiteamonad@users.noreply.github.com>
2019-05-27 12:18:28 +0100

    Implement fromString for Duration

--- datetimekt/src/main/java/Duration.kt
+++ datetimekt/src/main/java/Duration.kt
@@ -1,5 +1,6 @@
 import Consts.SECONDS_IN_AN_HOUR
 import Consts.SECONDS_IN_A_MINUTE
+import java.util.regex.Pattern
 
 class Duration constructor (
         totalSeconds: Int
@@ -10,6 +11,29 @@ class Duration constructor (
     operator fun plus(other: TimeType): Duration = Duration(this.getHours() + other.getHours(), this.getMinutes() + other.getMinutes(), this.getSeconds() + other.getSeconds())
     operator fun minus(other: TimeType): Duration = Duration(this.getHours() - other.getHours(), this.getMinutes() - other.getMinutes(), this.getSeconds() - other.getSeconds())
 
-    companion object
+    companion object {
+
+        private const val STRING_FORMAT_REGEX = """^\d+:\d{2}:\d{2}$"""
+
+        /**
+         * Converts a string in the format h:mm:ss as would be given by
+         * toString() into a Duration.
+         *
+         * The `h` portion of the string can have any non-zero length.
+         *
+         * Returns null if the string is invalid.
+         */
+        @JvmStatic
+        fun fromString(string: String): Duration? {
+            val pattern = Pattern.compile(STRING_FORMAT_REGEX)
+            return if (pattern.matcher(string).matches()) {
+                val parts = string.split(':')
+                Duration(parts[0].toInt(), parts[1].toInt(), parts[2].toInt())
+            } else {
+                null
+            }
+        }
+
+    }
 
 }
