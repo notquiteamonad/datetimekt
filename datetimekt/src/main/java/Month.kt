567f49f1cafca9d2c36899b95fbd5d5b05de938b
samueldple <samueldple@gmail.com>
2019-05-28 08:38:47 +0100

    Add month toString functions

--- datetimekt/src/main/java/Month.kt
+++ datetimekt/src/main/java/Month.kt
@@ -1,5 +1,7 @@
 import Consts.MAX_YEAR
 import Consts.MONTHS_IN_A_YEAR
+import java.time.LocalDate
+import java.util.*
 
 /**
  * Represents a month of a year between Jan 0000 and Dec 9999 inclusive.
@@ -30,11 +32,32 @@ class Month(
     fun getMonths(): Int = m
     fun getYears(): Int = y
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Month
+        if (m != other.m) return false
+        if (y != other.y) return false
+        return true
+    }
+
+    override fun hashCode(): Int = 31 * m + y
+
+    override fun toString(): String = String.format("%04d-%02d", y, m)
+
+    fun toReadableString(): String = String.format("%s %04d", MONTH_STRINGS[m - 1], y)
+
     companion object {
 
         private val MONTH_STRINGS = arrayOf(
                 "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
         )
+
+        @Suppress("unused")
+        fun thisMonth(): Month {
+            val date = LocalDate.now()
+            return Month(date.monthValue, date.year)
+        }
 
     }
 
