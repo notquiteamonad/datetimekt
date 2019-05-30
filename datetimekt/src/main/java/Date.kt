44ddbed354eafbb5e9f2c8ca7d29f8574f05ea68
samueldple <samueldple@gmail.com>
2019-05-30 14:18:05 +0100

    Add basic Date

---
+++ datetimekt/src/main/java/Date.kt
@@ -0,0 +1,127 @@
+import Utils.getLastDateInMonth
+import Utils.isLeapYear
+import java.time.LocalDate
+import java.util.regex.Pattern
+
+/**
+ * Represents a date between 1 Jan 0000 and 31 Dec 9999 inclusive.
+ *
+ * If d < 1 or d > the last day in the month, d will be set to 1 or the last day in the months respectively.
+ *
+ * If m < 0 or m > 12, 12 will be added or subtracted respectively until it is in the valid range.
+ *
+ * If y < 0 or y > 9999, it will be set to 0 or 9999 respectively.
+ */
+class Date(
+        d: Int,
+        m: Int,
+        y: Int
+): Comparable<Date> {
+
+    private var d: Int
+    private var m: Int
+    private var y: Int
+
+    init {
+        val month = Month(m, y)
+        var currentDateValue = d
+        if (currentDateValue < 1) {
+            currentDateValue = 1
+        } else if (currentDateValue > getLastDateInMonth(month.getMonth(), month.getYear())) {
+            currentDateValue = getLastDateInMonth(month.getMonth(), month.getYear())
+        }
+        this.d = currentDateValue
+        this.m = month.getMonth()
+        this.y = month.getYear()
+    }
+
+    fun getDate(): Int = d
+    fun getMonth(): Int = m
+    fun getYear(): Int = y
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Date
+        if (d != other.d) return false
+        if (m != other.m) return false
+        if (y != other.y) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = d
+        result = 31 * result + m
+        result = 31 * result + y
+        return result
+    }
+
+    override fun toString(): String = String.format("%04d-%02d-%02d", y, m, d)
+
+    /**
+     * Returns a String in the format 8 Jan 2019
+     */
+    fun toReadableString(): String = String.format("%d %s %04d", d, Month.MONTH_STRINGS[m - 1], y)
+
+    override fun compareTo(other: Date): Int =
+            if (y == other.getYear()) {
+                if (m == other.getMonth()) {
+                    d.compareTo(other.getDate())
+                } else {
+                    m.compareTo(other.getMonth())
+                }
+            } else {
+                y.compareTo(other.getYear())
+            }
+
+    /**
+     * Gets the number of days since 1 Jan 0000 (including that date and this one)
+     */
+    fun toDays(): Int {
+        var totalDays = 0;
+        for (currentY in 0 until y) {
+            totalDays += if (isLeapYear(currentY)) 366 else 365
+        }
+        for (currentM in 1 until m) {
+            totalDays += getLastDateInMonth(currentM, y)
+        }
+        totalDays += d
+        return totalDays
+    }
+
+    fun toMonth(): Month = Month.fromDate(this)
+
+    companion object {
+
+        private const val VALID_FORMAT_REGEX = """^\d{4}-\d{2}-\d{2}$"""
+
+        /**
+         * Gets the current date as a Date.
+         */
+        @JvmStatic
+        @Suppress("unused")
+        fun today(): Date {
+            val date = LocalDate.now()
+            return Date(date.dayOfMonth, date.monthValue, date.year)
+        }
+
+        /**
+         * Converts a string in the format yyyy-mm-dd as would be given by
+         * toString() into a Date.
+         *
+         * Returns null if the string is invalid.
+         */
+        @JvmStatic
+        fun fromString(string: String): Date? {
+            val pattern = Pattern.compile(VALID_FORMAT_REGEX)
+            return if (pattern.matcher(string).matches()) {
+                val parts = string.split('-')
+                Date(parts[2].toInt(), parts[1].toInt(), parts[0].toInt())
+            } else {
+                null
+            }
+        }
+
+    }
+
+}
---
+++ datetimekt/src/test/java/DateTests.kt
@@ -0,0 +1,60 @@
+import io.kotlintest.data.forall
+import io.kotlintest.matchers.boolean.shouldBeFalse
+import io.kotlintest.matchers.boolean.shouldBeTrue
+import io.kotlintest.matchers.withClue
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.kotlintest.tables.row
+
+class DateTests: StringSpec ({
+
+    "dates valid and invalid" {
+        forall(
+                row(2000, 12, 5, 2000, 12, 5),
+                row(2000, 12, 1, 2000, 12, 0),
+                row(2000, 12, 31, 2000, 12, 40),
+                row(2000, 1, 5, 2000, 13, 5),
+                row(9999, 5, 5, 10000, 5, 5)
+        ) { expectedY, expectedM, expectedD, inputY, inputM, inputD ->
+            val date = Date(inputD, inputM, inputY)
+            date.getDate() shouldBe expectedD
+            date.getMonth() shouldBe expectedM
+            date.getYear() shouldBe expectedY
+        }
+    }
+
+    "date to string" {
+        forall(
+                row("2000-05-01", "1 May 2000", 1, 5, 2000),
+                row("0050-01-01", "1 Jan 0050", 1, 1, 50),
+                row("9999-01-01", "1 Jan 9999", 1, 13, 10000)
+        ) { string, readableString, d, m, y ->
+            Date(d, m, y).toString() shouldBe string
+            Date(d, m, y).toReadableString() shouldBe readableString
+        }
+    }
+
+    "equality" {
+        Date(1, 5, 9) shouldBe Date(1, 5, 9)
+    }
+
+    "comparison" {
+            val dates = arrayOf(
+                    Date(5, 6, 2000),
+                    Date(5, 6, 2000),
+                    Date(4, 7, 2000),
+                    Date(1, 1, 2001)
+            )
+            withClue("comparison1") {(dates[0] <= dates[1]).shouldBeTrue()}
+            withClue("comparison2") {(dates[0] < dates[1]).shouldBeFalse()}
+            withClue("comparison3") {(dates[0] >= dates[1]).shouldBeTrue()}
+            withClue("comparison4") {(dates[0] < dates[2]).shouldBeTrue()}
+            withClue("comparison5") {(dates[2] < dates[3]).shouldBeTrue()}
+            withClue("comparison6") {(dates[3] > dates[1]).shouldBeTrue()}
+    }
+
+    "to month" {
+        Date(5, 6, 7).toMonth() shouldBe Month(6, 7)
+    }
+
+})
